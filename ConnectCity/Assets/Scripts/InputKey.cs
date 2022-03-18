6ad22af730c825afb6fdb389e2d1df8132f78ad5
hiroki6207 <hiroki6207@gmail.com>
2022-03-19 01:55:12 +0900

    2022/3/18_イメージ編集_フェードイン、フェードアウト実装_スクリプト追加

--- ConnectCity/Assets/Scripts/InputKey.cs
+++ ConnectCity/Assets/Scripts/InputKey.cs
@@ -4,10 +4,18 @@ using UnityEngine;
 
 public class Input : MonoBehaviour
 {
+    public GameObject title_image;
+    public GameObject push_image;
+    public GameObject gamestart_image;
+    public GameObject gamefinish_image;
+    public GameObject pause_pencil_image;
+    public GameObject gamefinish_check_image;
+    public GameObject yes_image;
+    public GameObject no_image;
     // Start is called before the first frame update
     void Start()
     {
-        
+        title_image = GameObject.Find("Title_Logo_Image");
     }
 
     // Update is called once per frame
