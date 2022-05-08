32c9546132196fa0dfadf6984a5cb740dc927b3e
hiroki6207 <hiroki6207@gmail.com>
2022-05-08 14:09:39 +0900

    2022/5/7_重力スクリプト更新途中

--- ConnectCity_gimmick_scene/Assets/Scripts/ChangeGravity.cs
+++ ConnectCity_gimmick_scene/Assets/Scripts/ChangeGravity.cs
@@ -1,11 +1,18 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
 
+[RequireComponent(typeof(Collider))]
 public class ChangeGravity : MonoBehaviour
 {
     [SerializeField] private Vector3 localGravity;
+    [SerializeField] private Direction direction;
+    [SerializeField] private TriggerEvent onTriggerEnter = new TriggerEvent();
+    [SerializeField] private TriggerEvent onTriggerStay = new TriggerEvent();
     private Rigidbody rBody;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -28,4 +35,37 @@ public class ChangeGravity : MonoBehaviour
     {
         rBody.AddForce(localGravity, ForceMode.Acceleration);
     }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        onTriggerEnter.Invoke(other);
+    }
+
+    /// <summary>
+    /// Is TriggerがONで他のColliderと重なっているときは、このメソッドが常にコールされる
+    /// </summary>
+    /// <param name="other"></param>
+
+    private void OnTriggerStay(Collider other)
+    {
+        //onTriggerStayで指定された処理を実行する
+        onTriggerStay.Invoke(other);
+    }
+
+    //UnityEventを継承したクラスに[Serializable]属性を付与することで、Inspectorウィンドウ上に表示できるようになる。
+    [Serializable]
+    public class TriggerEvent : UnityEvent<Collider>
+    { }
+}
+
+public enum Direction
+{
+    /// <summary>上</summary>
+    UP
+    /// <summary>下</summary>
+    , DOWN
+    /// <summary>左</summary>
+    , LEFT
+    /// <summary>右</summary>
+    , RIGHT
 }
