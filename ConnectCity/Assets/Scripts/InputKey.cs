b401390f2b9f5615a32c9b2bd22d3162c3ea956e
ktgitfrog333 <paraysis+git@gmail.com>
2022-05-10 13:49:13 +0900

    ▼修正対応 コードクリーン

--- ConnectCity/Assets/Scripts/InputKey.cs
+++ ConnectCity/Assets/Scripts/InputKey.cs
@@ -8,22 +8,25 @@ using Main.Common;
 
 namespace TitleSelect
 {
+    /// <summary>
+    /// ëÄçÏÉLÅ[ÇÃêßå‰
+    /// </summary>
     public class InputKey : MonoBehaviour
     {
-        public GameObject title_image;
-        public GameObject push_image;
-        public GameObject gamestart_image;
-        public GameObject gamefinish_image;
-        public GameObject pause_pencil_image;
-        public GameObject gamefinish_check_image;
-        public GameObject yes_image;
-        public GameObject no_image;
+        //[SerializeField] private GameObject title_image;
+        [SerializeField] private GameObject pushImage;
+        [SerializeField] private GameObject gamestartImage;
+        [SerializeField] private GameObject gamefinishImage;
+        [SerializeField] private GameObject pause_pencilImage;
+        [SerializeField] private GameObject gamefinishCheckImage;
+        [SerializeField] private GameObject yesImage;
+        [SerializeField] private GameObject noImage;
 
-        public Vector3 pencil_pos;
-        public Vector3 gamestart_pos;
-        public Vector3 gamefinish_pos;
-        public Vector3 yes_pos;
-        public Vector3 no_pos;
+        private Vector3 pencil_pos;
+        private Vector3 gamestart_pos;
+        private Vector3 gamefinish_pos;
+        private Vector3 yes_pos;
+        private Vector3 no_pos;
 
         public enum Title_Status
         {
@@ -45,30 +48,34 @@ namespace TitleSelect
         Title_Status status;
         Start_End start_end;
         Yes_No yes_no;
-        // Start is called before the first frame update
-        public void Start()
+
+        private void Reset()
         {
-            title_image = GameObject.Find("Title_Logo_Image");
-            push_image = GameObject.Find("PushGameStart_Logo_Image");
-            gamestart_image = GameObject.Find("GameStart_Logo_Image");
-            gamefinish_image = GameObject.Find("GameFinish_Logo_Image");
-            pause_pencil_image = GameObject.Find("Pause_Pencil_Logo_Image");
-            gamefinish_check_image = GameObject.Find("GameFinish_Check_Logo_Image");
-            yes_image = GameObject.Find("Yes_Logo_Image");
-            no_image = GameObject.Find("No_Logo_Image");
-            pencil_pos = pause_pencil_image.GetComponent<RectTransform>().anchoredPosition;
-            gamestart_pos = gamestart_image.GetComponent<RectTransform>().anchoredPosition;
-            gamefinish_pos = gamefinish_image.GetComponent<RectTransform>().anchoredPosition;
-            yes_pos = yes_image.GetComponent<RectTransform>().anchoredPosition;
-            no_pos = no_image.GetComponent<RectTransform>().anchoredPosition;
+            //title_image = GameObject.Find("Title_Logo_Image");
+            pushImage = GameObject.Find("PushGameStart_Logo_Image");
+            gamestartImage = GameObject.Find("GameStart_Logo_Image");
+            gamefinishImage = GameObject.Find("GameFinish_Logo_Image");
+            pause_pencilImage = GameObject.Find("Pause_Pencil_Logo_Image");
+            gamefinishCheckImage = GameObject.Find("GameFinish_Check_Logo_Image");
+            yesImage = GameObject.Find("Yes_Logo_Image");
+            noImage = GameObject.Find("No_Logo_Image");
+        }
+
+        private void Start()
+        {
+            pencil_pos = pause_pencilImage.GetComponent<RectTransform>().anchoredPosition;
+            gamestart_pos = gamestartImage.GetComponent<RectTransform>().anchoredPosition;
+            gamefinish_pos = gamefinishImage.GetComponent<RectTransform>().anchoredPosition;
+            yes_pos = yesImage.GetComponent<RectTransform>().anchoredPosition;
+            no_pos = noImage.GetComponent<RectTransform>().anchoredPosition;
             status = Title_Status.Push_Button;
-            push_image.SetActive(true);
-            gamestart_image.SetActive(false);
-            gamefinish_image.SetActive(false);
-            pause_pencil_image.SetActive(false);
-            gamefinish_check_image.SetActive(false);
-            yes_image.SetActive(false);
-            no_image.SetActive(false);
+            pushImage.SetActive(true);
+            gamestartImage.SetActive(false);
+            gamefinishImage.SetActive(false);
+            pause_pencilImage.SetActive(false);
+            gamefinishCheckImage.SetActive(false);
+            yesImage.SetActive(false);
+            noImage.SetActive(false);
         }
 
         // Update is called once per frame
@@ -77,32 +84,36 @@ namespace TitleSelect
             switch (status)
             {
                 case Title_Status.Push_Button:
+                    // èCê≥
                     if (Input.GetKeyDown(KeyCode.Space))
                     {
-                        push_image.SetActive(false);
-                        gamestart_image.SetActive(true);
-                        gamefinish_image.SetActive(true);
-                        pause_pencil_image.SetActive(true);
+                        pushImage.SetActive(false);
+                        gamestartImage.SetActive(true);
+                        gamefinishImage.SetActive(true);
+                        pause_pencilImage.SetActive(true);
                         start_end = Start_End.Start;
                         pencil_pos.x = gamestart_pos.x - 280;
                         pencil_pos.y = gamestart_pos.y;
-                        pause_pencil_image.GetComponent<RectTransform>().anchoredPosition = pencil_pos;
+                        pause_pencilImage.GetComponent<RectTransform>().anchoredPosition = pencil_pos;
                         status = Title_Status.Start_End;
                     }
                     break;
 
                 case Title_Status.Start_End:
+                    // èCê≥
                     if (start_end == Start_End.Start)
                     {
+                        // èCê≥
                         if (Input.GetKeyDown(KeyCode.DownArrow))
                         {
                             Debug.Log("â∫É{É^Éì");
                             Debug.Log(pencil_pos.x);
                             pencil_pos.x = gamefinish_pos.x - 275;
                             pencil_pos.y = gamefinish_pos.y;
-                            pause_pencil_image.GetComponent<RectTransform>().anchoredPosition = pencil_pos;
+                            pause_pencilImage.GetComponent<RectTransform>().anchoredPosition = pencil_pos;
                             start_end = Start_End.End;
                         }
+                        // èCê≥
                         if (Input.GetKeyDown(KeyCode.Space))
                         {
                             GameObject.Find("FadeInOutPanel").GetComponent<FadeInOut>().Fadeout();
@@ -112,35 +123,38 @@ namespace TitleSelect
                     }
                     else if (start_end == Start_End.End)
                     {
+                        // èCê≥
                         if (Input.GetKeyDown(KeyCode.UpArrow))
                         {
                             Debug.Log("è„É{É^Éì");
                             Debug.Log(pencil_pos.x);
                             pencil_pos.x = gamestart_pos.x - 280;
                             pencil_pos.y = gamestart_pos.y;
-                            pause_pencil_image.GetComponent<RectTransform>().anchoredPosition = pencil_pos;
+                            pause_pencilImage.GetComponent<RectTransform>().anchoredPosition = pencil_pos;
                             start_end = Start_End.Start;
                         }
+                        // èCê≥
                         if (Input.GetKeyDown(KeyCode.Space))
                         {
-                            gamestart_image.SetActive(false);
-                            gamefinish_image.SetActive(false);
-                            gamefinish_check_image.SetActive(true);
-                            yes_image.SetActive(true);
-                            no_image.SetActive(true);
+                            gamestartImage.SetActive(false);
+                            gamefinishImage.SetActive(false);
+                            gamefinishCheckImage.SetActive(true);
+                            yesImage.SetActive(true);
+                            noImage.SetActive(true);
                             yes_no = Yes_No.Yes;
                             pencil_pos.x = yes_pos.x - 100;
                             pencil_pos.y = yes_pos.y;
-                            pause_pencil_image.GetComponent<RectTransform>().anchoredPosition = pencil_pos;
+                            pause_pencilImage.GetComponent<RectTransform>().anchoredPosition = pencil_pos;
                             status = Title_Status.End_Confirm;
                         }
                     }
+                    // èCê≥
                     if (Input.GetKeyDown(KeyCode.Escape))
                     {
-                        gamestart_image.SetActive(false);
-                        gamefinish_image.SetActive(false);
-                        pause_pencil_image.SetActive(false);
-                        push_image.SetActive(true);
+                        gamestartImage.SetActive(false);
+                        gamefinishImage.SetActive(false);
+                        pause_pencilImage.SetActive(false);
+                        pushImage.SetActive(true);
                         status = Title_Status.Push_Button;
                     }
                     break;
@@ -148,13 +162,15 @@ namespace TitleSelect
                 case Title_Status.End_Confirm:
                     if (yes_no == Yes_No.Yes)
                     {
+                        // èCê≥
                         if (Input.GetKeyDown(KeyCode.DownArrow))
                         {
                             pencil_pos.x = no_pos.x - 180;
                             pencil_pos.y = no_pos.y;
-                            pause_pencil_image.GetComponent<RectTransform>().anchoredPosition = pencil_pos;
+                            pause_pencilImage.GetComponent<RectTransform>().anchoredPosition = pencil_pos;
                             yes_no = Yes_No.No;
                         }
+                        // èCê≥
                         if (Input.GetKeyDown(KeyCode.Space))
                         {
                             Quit();
@@ -162,24 +178,26 @@ namespace TitleSelect
                     }
                     else if (yes_no == Yes_No.No)
                     {
+                        // èCê≥
                         if (Input.GetKeyDown(KeyCode.UpArrow))
                         {
                             pencil_pos.x = yes_pos.x - 100;
                             pencil_pos.y = yes_pos.y;
-                            pause_pencil_image.GetComponent<RectTransform>().anchoredPosition = pencil_pos;
+                            pause_pencilImage.GetComponent<RectTransform>().anchoredPosition = pencil_pos;
                             yes_no = Yes_No.Yes;
                         }
+                        // èCê≥
                         if (Input.GetKeyDown(KeyCode.Space))
                         {
-                            gamefinish_check_image.SetActive(false);
-                            yes_image.SetActive(false);
-                            no_image.SetActive(false);
-                            gamestart_image.SetActive(true);
-                            gamefinish_image.SetActive(true);
+                            gamefinishCheckImage.SetActive(false);
+                            yesImage.SetActive(false);
+                            noImage.SetActive(false);
+                            gamestartImage.SetActive(true);
+                            gamefinishImage.SetActive(true);
                             start_end = Start_End.Start;
                             pencil_pos.x = gamestart_pos.x - 280;
                             pencil_pos.y = gamestart_pos.y;
-                            pause_pencil_image.GetComponent<RectTransform>().anchoredPosition = pencil_pos;
+                            pause_pencilImage.GetComponent<RectTransform>().anchoredPosition = pencil_pos;
                             status = Title_Status.Start_End;
                         }
                     }
